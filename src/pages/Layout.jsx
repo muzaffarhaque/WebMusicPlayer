a7df0dabe2bfb229240519ea752056fae1187452
muzaffarhaque <muzaffarhaque777@gmail.com>
2023-11-10 11:26:40 +0530

    Subject : some issur fixed -logr out icon aded -default arrow remove form otp

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Image } from 'react-bootstrap';
 import {Outlet, useNavigate} from 'react-router-dom';
 import settingImage from '../assets/images/setting.svg';
+import logout from '../assets/images/logout.svg';
 export default function Layout() {
     // const navigae = useNavigate();
     function rediractTo(){
@@ -9,11 +10,11 @@ export default function Layout() {
         location.reload();
     }
     return (
-        <div className='layout-main-wrapper d-flex min-vh-100'>
+        <div className='layout-main-wrapper align-items-lg-start position-relative d-flex h-100 min-vh-100'>
             <ul className="side-nave-bar m-0 p-0">
                 <li className='fs-36 primary fw-bold'>Logo</li>
                 <li className='active fs-14 fw-normal blue-189'> <Image src={settingImage} className='' alt='icon'/> Setting</li>
-              <li onClick={rediractTo}> Log Out</li>
+              <li onClick={rediractTo}><Image src={logout} alt='icon'/> Log Out</li>
             </ul>
             <div className="layout-right-main-wrapper">
                 <Outlet/>
